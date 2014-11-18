0ae678c0cdf377b304cccd584d8254925f9fd91d
quietlyapp <quietlyapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-18 23:50:52 +0000

    Tag version 1.3.3
    git-svn-id: https://plugins.svn.wordpress.org/quietly/tags/1.3.3@1028327 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-quietly-embed.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Quietly Embed Class
+ * Handles detection and insertion of Quietly embeds.
+ * @package Quietly
+ */
+
+class QuietlyEmbed {
+
+	/**
+	 * Quietly oEmbed endpoint url.
+	 * @var string
+	 */
+	protected $oembed_url = '';
+
+	/**
+	 * Quietly embed code regex.
+	 * @var string
+	 */
+	protected $embed_regex = '';
+
+	/**
+	 * Determines if the current output is a post excerpt.
+	 * @var boolean
+	 */
+	protected $is_excerpt = false;
+
+	/**
+	 * List of embed descriptions to output in place of excerpt.
+	 * @var array
+	 */
+	protected $embed_descriptions = array();
+
+	/**
+	 * Initializes the object.
+	 */
+	public function __construct() {
+		$this->oembed_url = 'http://' . QUIETLY_WP_URL . '/oembed';
+		$this->embed_regex = '#https?://(beta\.|www\.)?' . QUIETLY_WP_URL_DOMAIN . '/list/.*#i';
+		// Register embed handler
+		wp_embed_register_handler( QUIETLY_WP_SLUG, $this->embed_regex, array( $this, 'embed_register_handler' ) );
+		wp_oembed_add_provider( $this->embed_regex, $this->oembed_url, true );
+		// Register excerpt detection
+		add_filter( 'get_the_excerpt', array( $this, 'flag_excerpt' ), 0 );
+		add_filter( 'get_the_excerpt', array( $this, 'unflag_excerpt' ), 99 );
+		add_action( 'wp_footer', array( $this, 'display_footer' ) );
+	}
+
+	/**
+	 * Registers the Quietly embed handler for share url.
+	 */
+	public function embed_register_handler( $matches, $attr, $url, $rawattr ) {
+		$embed = '';
+		$url = $matches[0];
+		if ( strpos( $url, 'maxheight=' ) === false ) {
+			// Override WordPress default maxheight
+			$attr['height'] = (int) ((int) $attr['width'] * 0.75);
+		}
+		// Hide the list markups when showing in an excerpt
+		if ( true === $this->is_excerpt ) {
+			if ( QuietlyOptions::get_option( 'show_description_in_excerpts' ) === true) {
+				// Remember list description to be rendered in excerpt
+				require_once( ABSPATH . WPINC . '/class-oembed.php' );
+				$oembed = _wp_oembed_get_object();
+				$oembed = $oembed->fetch( $this->oembed_url, $url );
+				if (is_object($oembed) && property_exists($oembed, 'description')) {
+					array_push( $this->embed_descriptions, '<p>' . $oembed->description . '</p>' );
+				}
+			} else {
+				$embed = '';
+			}
+		} else {
+			$embed = wp_oembed_get( $url, $attr );
+		}
+		return apply_filters( 'embed_quietly', $embed, $matches, $attr, $url, $rawattr );
+	}
+
+	/**
+	 * Keeps track of whether an imminent embed output is for a post excerpt.
+	 * @param    string     $text    The excerpt text.
+	 */
+	public function flag_excerpt( $text = '' ) {
+		$this->is_excerpt = true;
+		$this->embed_descriptions = array();
+		return apply_filters( 'wp_trim_excerpt', $text, $text );
+	}
+
+	/**
+	 * Unflags the excerpt output status.
+	 * @param    string     $text    The excerpt text.
+	 */
+	public function unflag_excerpt( $text = '' ) {
+		$this->is_excerpt = false;
+		// Show list description if excerpt is empty
+		if ( $text === '') {
+			foreach ( $this->embed_descriptions as $embed) {
+				$text .= $embed;
+			}
+			$this->embed_descriptions = array();
+		}
+		return apply_filters( 'wp_trim_excerpt', $text, $text );
+	}
+
+	/**
+	 * Displays plugin info in the footer.
+	 */
+	public function display_footer() {
+		echo '<!-- QWP v' . QUIETLY_WP_VERSION . ' -->' . "\r\n";
+	}
+
+}
---
+++ quietly-wordpress.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Plugin Name: Quietly
+ * Plugin URI:  http://wordpress.org/plugins/quietly
+ * Description: The Quietly WP plug-in allows you to quickly and easily embed your Quietly content (e.g. beautiful slideshows, maps, etc) into your WordPress website.
+ * Version:     1.3.3
+ * Author:      Quietly Media, Inc.
+ * Author URI:  http://quiet.ly
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// Abort if called directly
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Plugin constants
+define( 'QUIETLY_WP_VERSION', '1.3.3' );
+define( 'QUIETLY_WP_SLUG', 'quietly' );
+define( 'QUIETLY_WP_SLUG_OPTIONS', QUIETLY_WP_SLUG . '_options' );
+define( 'QUIETLY_WP_URL_DOMAIN', 'quiet.ly' );
+define( 'QUIETLY_WP_URL', 'www.' . QUIETLY_WP_URL_DOMAIN );
+define( 'QUIETLY_WP_PATH_ABS', plugin_dir_url(__FILE__) );
+define( 'QUIETLY_WP_PATH_DIR', plugin_dir_path( __FILE__ ) );
+define( 'QUIETLY_WP_PATH_BASENAME', plugin_basename( __FILE__ ) );
+define( 'QUIETLY_WP_DEBUG', false );
+
+// Load plugin class
+require_once( 'includes/class-quietly.php' );
+require_once( 'includes/class-quietly-options.php' );
+require_once( 'includes/class-quietly-embed.php' );
+require_once( 'includes/class-quietly-api.php' );
+require_once( 'admin/class-quietly-admin.php' );
+require_once( 'admin/class-quietly-settings.php' );
+require_once( 'admin/class-quietly-list-insert.php' );
+
+register_activation_hook( __FILE__, array( 'Quietly', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Quietly', 'deactivate' ) );
+
+// Run the plugin
+function run_quietly() {
+	$plugin = new Quietly();
+}
+run_quietly();
